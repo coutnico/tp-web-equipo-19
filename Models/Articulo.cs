1caba13a1849832f668aa038ef5c6f466bd5269d
marianrp09 <marian_rp09@hotmail.com>
2024-05-11 14:07:27 -0300

    add new class (Article)

---
+++ Models/Articulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tp_web_equipo_19.Models
+{
+    public class Articulo
+    {
+        /* •	Código de artículo.
+       •	Nombre.
+       •	Descripción.
+       •	Marca(seleccionable de una lista desplegable).
+       •	Categoría(seleccionable de una lista desplegable.
+       •	Imagen.
+       •	Precio.*/
+
+
+        public int ID { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IDMarca { get; set; }
+        public int IDCategoria { get; set; }
+        public decimal Precio { get; set; }
+        public string ImagenURl { get; set; }
+
+
+
+
+        //DatosCargados
+        public Image ImagenCargada { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+
+        //FUNCIONES
+
+    }
+}
